e6b3b39c1ab3cfde23c16630c19bdf8823d5f73d
Dariusz Gilewicz <dariusz.g92@wp.pl>
2023-05-25 15:46:21 +0100

    Create and implement the logic responsible for purchasing a car.

--- src/test/java/pl/zajavka/integration/CarDealershipTest.java
+++ src/test/java/pl/zajavka/integration/CarDealershipTest.java
@@ -2,22 +2,42 @@ package pl.zajavka.integration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
+import pl.zajavka.business.*;
+import pl.zajavka.business.dao.CarDAO;
+import pl.zajavka.business.dao.CustomerDAO;
+import pl.zajavka.business.dao.SalesmanDAO;
 import pl.zajavka.business.management.CarDealershipManagementService;
 import pl.zajavka.business.management.FileDataPreparationService;
 import pl.zajavka.infrastructure.configuration.HibernateUtil;
 import pl.zajavka.infrastructure.database.repository.CarDealershipManagementRepository;
+import pl.zajavka.infrastructure.database.repository.CarRepository;
+import pl.zajavka.infrastructure.database.repository.CustomerRepository;
+import pl.zajavka.infrastructure.database.repository.SalesmanRepository;
 
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CarDealershipTest {
 
     private CarDealershipManagementService carDealershipManagementService;
+    private CarPurchaseService carPurchaseService;
 
     @BeforeEach
     void beforeEach(){
+
+        CarDAO carDAO = new CarRepository();
+        SalesmanDAO salesmanDAO = new SalesmanRepository();
+        CustomerDAO customerDAO = new CustomerRepository();
+        FileDataPreparationService fileDataPreparationService = new FileDataPreparationService();
+
         this.carDealershipManagementService = new CarDealershipManagementService(
                 new CarDealershipManagementRepository(),
-                new FileDataPreparationService()
+                fileDataPreparationService
+        );
+        this.carPurchaseService = new CarPurchaseService(
+                fileDataPreparationService,
+                new CustomerService(customerDAO),
+                new CarService(carDAO),
+                new SalesmanService(salesmanDAO)
         );
     }
     @AfterAll
@@ -43,12 +63,14 @@ public class CarDealershipTest {
     @Order(3)
     void purchase() {
         log.info("### RUNNING ORDER 3");
+        carPurchaseService.purchase();
     }
 
     @Test
     @Order(4)
     void makeServiceRequests() {
         log.info("### RUNNING ORDER 4");
+
     }
 
     @Test
