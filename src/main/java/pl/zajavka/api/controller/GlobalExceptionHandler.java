33ab71f98dbe7e0692fef3cf11081a1ed72653e4
Dariusz Gilewicz <dariusz.g92@wp.pl>
2023-06-12 16:02:04 +0100

    Create service controller and mapping logic for viewing service endpoint.

--- src/main/java/pl/zajavka/api/controller/GlobalExceptionHandler.java
+++ src/main/java/pl/zajavka/api/controller/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package pl.zajavka.api.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import pl.zajavka.domain.exception.ProcessingException;
 
 @Slf4j
 @ControllerAdvice
@@ -12,6 +15,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ModelAndView handleException(Exception ex){
         String message = String.format("Other exception occurred: [%s]", ex.getMessage());
+        log.error(message, ex);
+        ModelAndView modelView = new ModelAndView("error");
+        modelView.addObject("errorMessage", message);
+        return modelView;
+    }
+
+    @ExceptionHandler(ProcessingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handleException(ProcessingException ex){
+        String message = String.format("Processing exception occurred: [%s]", ex.getMessage());
         log.error(message, ex);
         ModelAndView modelView = new ModelAndView("error");
         modelView.addObject("errorMessage", message);
